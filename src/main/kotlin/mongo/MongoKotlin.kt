bc5a7e4b7134e83b6bb859d5e7d0f93c9b1db5d2
Phillip Markwick <30838325+phillipmarkwick@users.noreply.github.com>
2024-04-18 07:11:41 +0100

    typeo

--- src/main/kotlin/mongo/MongoKotlin.kt
+++ src/main/kotlin/mongo/MongoKotlin.kt
@@ -10,7 +10,7 @@ import org.bson.codecs.kotlin.DataClassCodecProvider
 
 object Mdb {
     private const val CONNECTION_STRING = "YOUR_CONNECTION_STRING"
-    private const val DATABASE_NAME = "YOUR_DATABASENAME"
+    private const val DATABASE_NAME = "YOUR_DATABASE NAME"
     private val clientSettings = MongoClientSettings.builder()
         .applyConnectionString(ConnectionString(CONNECTION_STRING))
         .codecRegistry(
